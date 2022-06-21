b4dfa5f31f5296dc10f9fc15d280df8967717079
jhseo2 <jh.seo@hanwha.com>
2022-06-21 08:05:17 +0000

    test

--- app.py
+++ app.py
@@ -9,6 +9,5 @@ def hello_world():
     name = request.args.get('name', 'World')
     return 'Hello {}!\n'.format(name)
 
-
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
